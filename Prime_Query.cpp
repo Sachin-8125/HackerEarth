7feae9b6b42bca7918a4ce8e7e99ecaef92fde9b
Sachin-8125 <devinewolf7@gmail.com>
2025-08-22 17:15:17 +0530

    Prime Query problem solved

--- Prime_Query.cpp
+++ Prime_Query.cpp
@@ -1,68 +1,66 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-#define int long long int
 
+using int64 = long long;  // safer alias instead of #define
+
+// Function to process each test case
 void solve() {
     int n;
     cin >> n;
+
     vector<int> a(n);
-    
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    
+
+    // Prefix sums for zeros and ones
     vector<int> zero(n, 0), one(n, 0);
-    for(int i = 0; i < n; i++) {
-        if(a[i] == 0) {
-            zero[i] = 1;
-        }
-        if(a[i] == 1) {
-            one[i] = 1;
-        }
-        if(i > 0) {
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) zero[i] = 1;
+        if (a[i] == 1) one[i] = 1;
+
+        if (i > 0) {
             zero[i] += zero[i - 1];
             one[i] += one[i - 1];
         }
     }
-    
+
     int q;
     cin >> q;
-    while(q--) {
+
+    while (q--) {
         int l, r;
         cin >> l >> r;
-        l = l - 1;
-        r = r - 1;
-        
-        int x = zero[r];
-        if(l > 0) {
-            x = x - zero[l - 1];
-        }
-        
-        int y = one[r];
-        if(l > 0) {
-            y = y - one[l - 1];
-        }
-        
-        int len = r - l + 1;
-        int c = (len * (len - 1)) / 2;
-        c = c - (x * (x - 1)) / 2 - (x * y);
-        cout << c << endl;
+
+        l--;  // convert to 0-based index
+        r--;
+
+        // Count zeros in [l, r]
+        int x = zero[r] - (l > 0 ? zero[l - 1] : 0);
+        // Count ones in [l, r]
+        int y = one[r] - (l > 0 ? one[l - 1] : 0);
+
+        // Total possible pairs
+        int64 len = r - l + 1;
+        int64 totalPairs = (len * (len - 1)) / 2;
+
+        // Subtract invalid pairs (only zeros together + zero-one pairs)
+        int64 invalidPairs = (x * (x - 1)) / 2 + (x * y);
+
+        cout << totalPairs - invalidPairs << "\n";
     }
 }
 
-#undef int
 int main() {
-    #define int long long int
     ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    
+    cin.tie(nullptr);
+
     int T;
     cin >> T;
-    for(int tc = 1; tc <= T; tc++) {
-        // cout << "Case #" << tc << ": ";
+    while (T--) {
         solve();
     }
+
     return 0;
 }
