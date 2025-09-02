549e14020572a957ffcaaaff8c6f5d9f01a9bf3d
Sachin-8125 <devinewolf7@gmail.com>
2025-09-02 17:19:09 +0530

    Souvik and Permutation

--- Special_Substrings.cpp
+++ Special_Substrings.cpp
@@ -12,34 +12,24 @@ int main() {
     cin >> S;
 
     long long result = 0;
-
-    // Store remainder -> count of substrings ending at current index
     unordered_map<long long, long long> prev, curr;
 
     for (int i = 0; i < N; i++) {
         curr.clear();
         int digit = S[i] - '0';
-
-        // Start new substring with this digit
         long long rem = digit % M;
         curr[rem]++;
-
-        // Extend previous substrings
         for (auto &p : prev) {
             long long new_rem = (p.first * 10 + digit) % M;
             curr[new_rem] += p.second;
         }
-
-        // Count substrings where gcd(value, M) == 1
         for (auto &p : curr) {
             if (__gcd(p.first, M) == 1) {
                 result += p.second;
             }
         }
-
         prev = curr;
     }
-
     cout << result << "\n";
     return 0;
 }
