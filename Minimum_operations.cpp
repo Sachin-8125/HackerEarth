88db185ea6ca9e2320fc123fbd13c4e5bdaf3c84
Sachin-8125 <devinewolf7@gmail.com>
2025-09-12 21:16:33 +0530

    Minimum Operations problem solved

--- Minimum_operations.cpp
+++ Minimum_operations.cpp
@@ -1,28 +1,48 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
+long long solve(int N, vector<int>& A) {
+    long long sum_A=0;
+    for (int i = 0; i < N; i++) {
+        sum_A += A[i];
+    }
+    if (sum_A > 0) return -1;
+    long long k = -sum_A;
+    bool all_zero = true;
+    for (int i = 0; i < N; i++) {
+        if (A[i] != 0) {
+            all_zero = false;
+            break;
+        }
+    }
+    if (all_zero) return 0;
+    if (k == 0) return -1;
+    long long min_x_sum = 0;
+    for (int i = 0; i < N; i++) {
+        if (A[i] >= 0) {
+            continue;
+        } else {
+            long long min_x_i = (-A[i] + 1) / 2;
+            min_x_sum += min_x_i;
+        }
+    }
+    if (min_x_sum > k) {
+        return -1;
+    }
+    return k;
+}
+
+int main() {
     int T;
     cin >> T;
     while (T--) {
         int N;
         cin >> N;
-        vector<long long> A(N);
-        long long sum = 0;
-        for (int i = 0; i < N; ++i) {
+        vector<int> A(N);
+        for (int i = 0; i < N; i++) {
             cin >> A[i];
-            sum += A[i];
         }
-        if (sum >= 0) {
-            cout << -1 << '\n';
-        } else {
-            cout << -sum << '\n';
-        }
+        cout << solve(N, A) << endl;
     }
-}
-
-int main() {
-    solve();
     return 0;
 }
