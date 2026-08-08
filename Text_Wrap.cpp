cad13e0a4ca1cc14ff41236139870297ac020465
Sachin-8125 <devinewolf7@gmail.com>
2026-08-09 03:19:15 +0530

    Refactor Text_Wrap solution for improved efficiency and correctness

--- Text_Wrap.cpp
+++ Text_Wrap.cpp
@@ -4,60 +4,34 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int n, L;
-    cin >> n >> L;
-    vector<int> words(n);
-    for (int i = 0; i < n; i++) {
-        cin >> words[i];
+    int n, k;
+    if(!(cin >> n >> k)) return 0;
+    vector<long long> a(n);
+    long long mx = 0, sum = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        mx = max(mx, a[i]);
+        sum += a[i];
     }
-    
-    // Binary search on the minimum width W
-    int left = *max_element(words.begin(), words.end());
-    int right = 0;
-    for (int len : words) right += len;
-    right += n - 1; // maximum possible spaces
-    
-    int ans = right;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        
-        // Check if we can fit all words in <= L lines with width mid
+    long long lo = mx, hi = sum + (n - 1);
+    auto good = [&](long long w) {
         int lines = 1;
-        int current_line = 0;
-        bool can_fit = true;
-        
-        for (int i = 0; i < n; i++) {
-            // Try to add word[i] to current line
-            if (current_line == 0) {
-                // First word in line
-                current_line = words[i];
+        long long cur = a[0];
+        for (int i = 1; i < n; ++i) {
+            if (cur + 1 + a[i] <= w) {
+                cur += 1 + a[i];
             } else {
-                // Check if word fits with 1 space gap
-                if (current_line + 1 + words[i] <= mid) {
-                    current_line += 1 + words[i];
-                } else {
-                    // Start new line
-                    lines++;
-                    current_line = words[i];
-                    
-                    // Check if single word fits in new line
-                    if (words[i] > mid) {
-                        can_fit = false;
-                        break;
-                    }
-                }
+                ++lines;
+                cur = a[i];
             }
         }
-        
-        if (can_fit && lines <= L) {
-            ans = mid;
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
+        return lines <= k;
+    };
+    while (lo < hi) {
+        long long mid = (lo + hi) >> 1;
+        if (good(mid)) hi = mid;
+        else lo = mid + 1;
     }
-    
-    cout << ans << '\n';
+    cout << lo << "\n";
     return 0;
 }
